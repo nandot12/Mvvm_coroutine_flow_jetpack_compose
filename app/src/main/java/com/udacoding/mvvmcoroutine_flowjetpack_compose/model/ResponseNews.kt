b181ce61f1ce7989196c6a422709782bdcc6061a
udacodingid <mentoring@udacoding.com>
2021-09-26 13:39:26 +0700

    push

--- app/src/main/java/com/udacoding/mvvmcoroutine_flowjetpack_compose/model/ResponseNews.kt
+++ app/src/main/java/com/udacoding/mvvmcoroutine_flowjetpack_compose/model/ResponseNews.kt
@@ -8,7 +8,7 @@ data class ResponseNews(
 	val totalResults: Int? = null,
 
 	@field:SerializedName("articles")
-	val articles: List<ArticlesItem?>? = null,
+	val articles: List<ArticlesItem>? = null,
 
 	@field:SerializedName("status")
 	val status: String? = null
